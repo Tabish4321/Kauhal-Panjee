f6ed26a41196eb1a170279de2a64cc0c3cb883b4
ajitranjan <saxena0794@gmail.com>
2026-07-06 16:53:58 +0530

    code commit and use 26 july 2026

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -371,19 +371,9 @@ dependencies {
     implementation("com.google.firebase:firebase-config-ktx")
 
 //     Ajit Ranjan Implimentation
-
-
-
     implementation("androidx.compose.runtime:runtime-livedata:1.6.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    // Hilt Navigation Compose
-    implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
-    // For ViewModel
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation ("androidx.compose.material:material-icons-extended")
-
+    implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
 
 
 }
