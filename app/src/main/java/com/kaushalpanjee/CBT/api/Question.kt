60177ac7f484d38be6ec15595acdaf8ae02dfeb9
ajitranjan <saxena0794@gmail.com>
2026-07-08 20:11:28 +0530

    code commit and use 08 july 2026 time 08:11 PM

--- app/src/main/java/com/kaushalpanjee/CBT/api/Question.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.CBT.api
-
-data class Question(
-    val question_id: String,
-    val question_value: String,
-    val option: List<Option>,
-    val marks_per_qs: Double
-)
