f2353c5690a2581571ea8c4d6f7940afb6bb0c6a
Tabish <tjamal333@gmail.com>
2026-07-27 10:31:21 +0530

    create error msg fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 62
-        //versionName = "2.7.3"
-        versionName = "2.6.6"
+        versionName = "2.7.4"
+       // versionName = "2.6.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
