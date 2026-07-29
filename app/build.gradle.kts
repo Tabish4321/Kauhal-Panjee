84d8820d7fb0210b3a9f829c708b8b0738d72cfe
Tabish <tjamal333@gmail.com>
2026-07-29 10:29:16 +0530

    merge CBT

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,9 @@ android {
         applicationId = "com.kaushalpanjee"
         minSdk = 28
         targetSdk = 35
-        versionCode = 62
-       // versionName = "2.7.4"
-        versionName = "2.6.6"
+        versionCode = 64
+        versionName = "2.7.4"
+       // versionName = "2.6.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
